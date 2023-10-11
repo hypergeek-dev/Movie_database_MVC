d3f9ab0c0600ffb45d9471007b242a159c14b6c4
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-11 20:00:13 +0200

    Add project files.

---
+++ MovieMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieMVC.Data;
+using MovieMVC.Models;
+using SQLitePCL;
+using System.Diagnostics;
+
+namespace MovieMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MovieMVCContext _context;
+
+        public HomeController(ILogger<HomeController> logger, MovieMVCContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        private List<Post> GetAllPosts()
+        {
+            List<Post> posts = _context.Posts.OrderByDescending(p => p.ReleaseDate).ToList();
+            return posts;
+        }
+        public IActionResult Index()
+        {
+            List<Post> posts = GetAllPosts();
+            return View(posts);
+        }
+        
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
